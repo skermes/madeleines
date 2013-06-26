efb6888ef27196426918fc0fd175bf23bfbd3e0b
Sean Kermes <skermes@gmail.com>
2013-06-26 12:55:26 -0400

    Initial commit.  `lein new compojure madeleines`.

---
+++ test/madeleines/test/handler.clj
@@ -0,0 +1,14 @@
+(ns madeleines.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        madeleines.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
