326f21802069fa0646c3b6f1fbc4f9ebf1a3137c
Sean Kermes <skermes@gmail.com>
2013-06-26 14:27:31 -0400

    Layout and format viewing page.

--- src/madeleines/views.clj
+++ src/madeleines/views.clj
@@ -1,11 +1,36 @@
 (ns madeleines.views
-  (:use [hiccup core page]))
+  (:use madeleines.data)
+  (:use [hiccup core page element]))
 
-(defn index-page []
+(defn- footer [] [:div {:class "footer"}])
+
+(defn- header [] [:h1 "Madeleines"])
+
+(defn- head-tag []
+  [:head
+    [:title "Madeleines"]
+    (include-css "/css/madeleines.css")])
+
+(defn- layout [& content]
   (html5
-    [:head
-      [:title "Madeleines"]
-      (include-css "/css/madeleines.css")]
+    (head-tag)
     [:body
       [:div {:class "container"}
-        [:h1 "Madeleines"]]]))
+        (header)
+        content
+        (footer)]]))
+
+(defn index-page []
+  (let [{title :title url :url preview :preview} (bite)]
+    (layout
+      (link-to url [:h2 title])
+      [:div {:class "preview"}
+        preview])))
+
+
+
+
+
+
+
+
