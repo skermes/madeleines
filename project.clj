353726c03942313fd30e1c68e70c5c89b6527b93
Sean Kermes <skermes@gmail.com>
2013-06-26 22:48:05 -0400

    Depend on Korma and postgres.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [hiccup "1.0.0"]]
+                 [hiccup "1.0.0"]
+                 [korma "0.3.0-RC5"]
+                 [postgresql "9.1-901.jdbc4"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler madeleines.handler/app}
   :profiles
