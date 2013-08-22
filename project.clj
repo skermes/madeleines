48470889a2071170c7c5c9f3066acb9e74ccfc12
Sean Kermes <skermes@gmail.com>
2013-08-21 23:24:52 -0400

    Put description in project file.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject madeleines "0.1.0"
-  :description "FIXME: write description"
+  :description "A very simple anti-bookmarking service."
   :url "http://madeleines.herokuapp.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
