5dc69af19276eb678333022f219bd96a0d93b705
Sean Kermes <skermes@gmail.com>
2013-08-21 23:24:25 -0400

    Set url in project.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject madeleines "0.1.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://madeleines.herokuapp.com"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.0"]
