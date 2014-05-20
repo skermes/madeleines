03a94d73b9385cfef3b8b346885746e1fab1ccbe
Sean Kermes <skermes@gmail.com>
2014-05-20 10:04:12 -0400

    Define json output for java.sql.Date.

--- src/madeleines/data/time.clj
+++ src/madeleines/data/time.clj
@@ -1,6 +1,8 @@
 (ns madeleines.data.time
   (:use [clj-time.core :only [date-time year month day now]])
-  (:use clj-time.coerce))
+  (:use clj-time.coerce)
+  (:import java.sql.Date)
+  (:require [clojure.data.json :refer [JSONWriter -write]]))
 
 (defn today-long []
   (let [n (now)]
@@ -8,3 +10,8 @@
 
 (defn today-random [n]
   (.nextInt (java.util.Random. (today-long)) n))
+
+(extend-protocol JSONWriter
+  java.sql.Date
+  (-write [object out]
+    (-write (.getTime object) out)))
