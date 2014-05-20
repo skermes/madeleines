18826296643de1a96a689d30943ab686e7b2722e
Sean Kermes <skermes@gmail.com>
2014-05-20 10:03:35 -0400

    Include data.json in project.

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [postgresql "9.1-901.jdbc4"]
                  [clj-time "0.5.1"]
                  [com.gravity/goose "2.1.22"]
-                 [ring/ring-jetty-adapter "1.2.0"]]
+                 [ring/ring-jetty-adapter "1.2.0"]
+                 [org.clojure/data.json "0.2.4"]]
   :min-lein-version "2.2.0"
   :uberjar-name "madeleines-standalone.jar"
   :plugins [[lein-ring "0.8.5"]]
