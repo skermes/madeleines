7dd2fc3752b47fd346968edeb46c54de25b217fc
Sean Kermes <skermes@gmail.com>
2014-05-20 10:04:34 -0400

    Define API routes.

--- src/madeleines/handler.clj
+++ src/madeleines/handler.clj
@@ -2,6 +2,7 @@
   (:use compojure.core)
   (:use madeleines.views)
   (:use madeleines.data)
+  (:require [clojure.data.json :as json])
   (:require [compojure.handler :as handler]
             [compojure.route :as route])
   (:require [ring.adapter.jetty :as jetty]))
@@ -11,6 +12,12 @@
   (DELETE "/" [] (index-page (drop-bite)))
   (GET "/bake" [] (bake-page))
   (POST "/bake" [url] (bake-page (bake url)))
+
+  (context "/api/v1" []
+    (GET "/bite" [] (json/write-str (bite)))
+    (POST "/drop" [] (json/write-str (drop-bite)))
+    (POST "/bake" [url] (json/write-str {:status (bake url)})))
+
   (route/resources "/")
   (route/not-found (four-oh-four-page)))
 
