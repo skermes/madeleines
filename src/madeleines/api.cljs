faa24219252eb76feab98bd544675c56e6d34c26
Sean Kermes <skermes@gmail.com>
2014-05-21 00:41:17 -0400

    Move network code into own namespace.

---
+++ src/madeleines/api.cljs
@@ -0,0 +1,25 @@
+(ns madeleines.api
+  (:require [madeleines.app-state :as app-state]
+            [clojure.string :refer [replace]]
+            [goog.net.XhrIo]))
+
+(defn keywordify [s]
+  (-> s
+      (replace "_" "-")
+      keyword))
+
+(defn keywordify-keys [m]
+  (let [ks (keys m)
+        vs (vals m)]
+    (zipmap (map keywordify ks) vs)))
+
+(defn ajax->clj [response]
+  (->> response
+       (.-target)
+       (.getResponseJson)
+       js->clj
+       keywordify-keys))
+
+(defn fetch-todays-remembrance! []
+  (.send goog.net.XhrIo "/api/v1/bite"
+                         #(app-state/new-remembrance! (ajax->clj %))))
--- src/madeleines/client.cljs
+++ src/madeleines/client.cljs
@@ -1,29 +1,9 @@
 (ns madeleines.client
-  (:require [madeleines.app-state :as app-state]
-            [madeleines.components :as components]
-            [reagent.core :as reagent]
-            [clojure.string :refer [replace]]
-            [goog.net.XhrIo]))
+  (:require [madeleines.components :as components]
+            [madeleines.api :as api]
+            [reagent.core :as reagent]))
 
 (defn ^:export run []
-  (reagent/render-component [components/app] (.-body js/document)))
-
-(defn keywordify [s]
-  (-> s
-      (replace "_" "-")
-      keyword))
-
-(defn keywordify-keys [m]
-  (let [ks (keys m)
-        vs (vals m)]
-    (zipmap (map keywordify ks) vs)))
-
-(defn load-ajax-json [response]
-  (->> response
-       (.-target)
-       (.getResponseJson)
-       js->clj
-       keywordify-keys))
-
-(.send goog.net.XhrIo "/api/v1/bite"
-                      #(app-state/new-remembrance! (load-ajax-json %)))
+  (do
+    (reagent/render-component [components/app] (.-body js/document))
+    (api/fetch-todays-remembrance!)))
