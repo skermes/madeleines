0ecf5c55e581d0a0f17c82ff02be07b51fdb8ab4
Sean Kermes <skermes@gmail.com>
2014-05-20 21:56:34 -0400

    Set up reagent atom with dummy data.
    Probably going to want to break the json parsing pipeline
    and keywordify-keys out into their own namespace eventually.

--- src/madeleines/client.cljs
+++ src/madeleines/client.cljs
@@ -1,3 +1,27 @@
-(ns madeleines.client)
+(ns madeleines.client
+  (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.string :refer [replace]]))
 
-(.log js/console "Hello, World")
+(def remembrance (atom nil))
+
+(defn keywordify [s]
+  (-> s
+      (replace "_" "-")
+      keyword))
+
+(defn keywordify-keys [m]
+  (let [ks (keys m)
+        vs (vals m)]
+    (zipmap (map keywordify ks) vs)))
+
+;; TODO: Replace with network code
+(->> "{\"dropped_on\": 1400558400000,
+       \"remembered_on\": 1377057600000,
+       \"preview\": \"A woman is abducted and hypnotized with material harvested from a flower. When she falls for a man, the two realize they have been subjected to the same process. Together, they search for safety as they struggle to reassemble their wrecked lives.\",
+       \"title\": \"Watch Upstream Color Online\",
+       \"url\": \"http://movies.netflix.com/WiMovie/Upstream_Color/70265224?trkid=13462069\",
+       \"id\": 4}"
+     (.parse js/JSON)
+     js->clj
+     keywordify-keys
+     (reset! remembrance))
