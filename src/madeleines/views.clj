77035edf9baa9439c767104d8924e9f4ca082f4d
Sean Kermes <skermes@gmail.com>
2014-05-22 16:51:44 -0400

    Remove now-redundtant views, partials and layouts.

--- src/madeleines/views.clj
+++ src/madeleines/views.clj
@@ -1,7 +1,5 @@
 (ns madeleines.views
-  (:use [hiccup core page element form])
-  (:use madeleines.views.partials)
-  (:use madeleines.views.layouts))
+  (:use [hiccup core page element form]))
 
 (defn js-client-page []
   (html5
@@ -12,27 +10,3 @@
       (include-js "/js/madeleines.js")
       [:script {:type "text/javascript"}
         "madeleines.client.run()"]]))
-
-(defn index-page [bite]
-  (let [{title :title url :url preview :preview dropped-on :dropped_on} bite]
-    (layout "index"
-      [:h2 (link-to url title)]
-      [:div {:class "preview"} preview]
-      [:div {:class "actions"} (drop-button-or-notice dropped-on)])))
-
-(defn bake-page
-  ([] (bake-page nil))
-  ([state]
-    (layout "bake"
-      (submission-state-toast state)
-      (form-to [:post "/bake"]
-        (text-field "url")
-        (submit-button "bake")))))
-
-(defn four-oh-four-page []
-  (layout "404"
-    (poem "Yesterday upon the stair"
-          "I saw a man who wasn't there"
-          "He wasn't there again today"
-          "I wish that man would go away")
-    [:div {:class "poet"} "Hughes Means"]))
