a4fee9795cb33dce5c6a46294270ea3e7e9002c3
Sean Kermes <skermes@gmail.com>
2014-10-16 23:05:24 -0400

    Rails new.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Madeleines::Application.config.secret_key_base = '7a5a24d5c56cce8087b1345e4d1393ab381de7154162336a6f393e2b79a6b2a409fd1fbe47bba038eeda3aa1370d2c46197162d97d4a04bb16c4d4fd813fcbeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Madeleines::Application.config.session_store :cookie_store, key: '_madeleines_session'
