d8b507793fc7222e0e67a4eb03a96174d37be487
Sean Kermes <skermes@gmail.com>
2014-10-17 00:18:27 -0400

    Methods for selecting a random remembrance.

--- app/models/remembrance.rb
+++ app/models/remembrance.rb
@@ -1,2 +1,19 @@
+CANDIDATE_QUERY = 'remembered_on < ? and
+                   (dropped_on is null or dropped_on >= ?)'
+
 class Remembrance < ActiveRecord::Base
+  def self.todays_candidates
+    Remembrance.where(CANDIDATE_QUERY, Date.today, Date.today)
+  end
+
+  def self.todays_prng
+    Random.new(Date.today.to_datetime.to_i)
+  end
+
+  def self.bite
+    candidates = Remembrance.todays_candidates
+    random = self.todays_prng
+    index = random.rand(candidates.count)
+    candidates[index]
+  end
 end
