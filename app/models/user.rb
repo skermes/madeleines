34642d8bc3f37317a0e5fdc8e4edc5af6ce196c6
Sean Kermes <skermes@gmail.com>
2014-10-31 12:44:26 -0400

    Validate unique emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :remembrances, :inverse_of => :user
+  validates_uniqueness_of :email, :case_sensitive => false
 
   def self.find_by_email_insensitive(email)
     nil if email.nil?
