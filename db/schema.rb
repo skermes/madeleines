0a581b7d26f15541ffa3d9703463949b9ddd98c0
Sean Kermes <skermes@gmail.com>
2015-02-22 19:25:26 -0500

    Add json blob for settings to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029002003) do
+ActiveRecord::Schema.define(version: 20150223002326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,13 +26,14 @@ ActiveRecord::Schema.define(version: 20141029002003) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                        null: false
+    t.string   "email",                                     null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_me_token"
     t.datetime "remember_me_token_expires_at"
+    t.json     "settings",                     default: {}
   end
 
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
