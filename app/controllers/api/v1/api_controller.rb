c618fbae5bd444850ba219f4a70636bed30091b5
Sean Kermes <skermes@gmail.com>
2015-03-05 15:33:15 -0500

    Allow API requests to bypass authenticity token.
    They all still require login, so they have to have an API key or
    remember_me cookie or whatever.  This just allows us to make
    requests from apps that don't have a working session (e.g., browser
    plugins).

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ApiController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def render_json(data, status=200)
     json = {'_authenticity_token' => form_authenticity_token, '_data' => data}
     render :json => json, :status => status
